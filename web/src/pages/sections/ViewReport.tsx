0b0ca5fb2f5569021183da4571df4ccb97d98a6d
Fonlogen <info.fonlogen@gmail.com>
2024-10-08 14:47:20 +0200

    uiw editor preview to show markdown

--- web/src/pages/sections/ViewReport.tsx
+++ web/src/pages/sections/ViewReport.tsx
@@ -2,7 +2,18 @@ import React, { useContext } from 'react'
 import { PuffLoader } from 'react-spinners';
 import { MDTContext } from '../App';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { 
+  faEye,
+  faLocationDot,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+
+import MDEditor, { selectWord } from "@uiw/react-md-editor";
+// No import is required in the WebPack.
+import "@uiw/react-md-editor/markdown-editor.css";
+// No import is required in the WebPack.
+import "@uiw/react-markdown-preview/markdown.css";
+
 
 interface ViewReportProps {
   report: number | any;
@@ -19,6 +30,17 @@ function ViewReport({ report }: ViewReportProps) {
 
   const { data, search, setSelectedData, setActiveComponent, theme } = context;
 
+  const loadCitizenComponent = async (citizen: any, theme: string) => {
+    const { default: CitizenView } = await import('./CitizenView');
+    setActiveComponent({
+      component: CitizenView,
+      props: {
+        citizen: citizen,
+        theme: theme,
+      },
+    });
+  };
+    
   return (
     <>
       {data === null && (
@@ -31,7 +53,7 @@ function ViewReport({ report }: ViewReportProps) {
         report = typeof report === 'number' ? data.reports[report] : report,
         <div className="flex flex-col w-full h-full px-2 py-1 gap-3">
           <div className="flex flex-row justify-between items-center gap-3 text-white border-b border-gray-700 pb-4">
-            <h2 className="text-2xl font-bold max-w-[60%]">Report #{report?.id} | {report?.title}</h2>
+            <h2 className="text-2xl font-bold flex-1 text-start">Report #{report?.id} | {report?.title}</h2>
             <div className='report-tags flex flex-row items-center gap-1'>
               {report?.tags &&
                 report?.tags?.map((tag: any, index: number) => {
@@ -50,8 +72,39 @@ function ViewReport({ report }: ViewReportProps) {
               }
             </div>
           </div>
-          <div className="flex flex-col gap-3 rounded-lg text-start flex-1 overflow-y-auto bg-[#222222] py-2 px-3 text-lg text-gray-300">
-            {report?.description}
+          <div className="flex flex-col gap-3 rounded-lg text-start flex-1 overflow-y-auto bg-[#222222] text-lg text-gray-300" data-color-mode="dark">
+            <MDEditor value={report?.description} preview="preview" hideToolbar={true} height={'100%'} visibleDragbar={false} />
+          </div>
+          <div className='flex flex-row gap-2 items-center justify-start pt-2 border-t border-gray-600 h-10'>
+            <h2 className='text-lg text-white'>Cittadini coinvolti</h2>
+            <div className='text-sm px-1 py-1 bg-[#222222] text-white rounded-lg h-full flex flex-row flex-1 overflow-x-auto gap-1'>
+              { !report?.involved || Object.keys(report?.involved).length === 0 && (
+                <span className='text-white'>Nessun cittadino coinvolto</span>
+              )}
+
+              {report?.involved &&
+                report?.involved.map((involved: any, index: number, cid: number) => (
+                  cid = involved,
+                  involved = data?.citizens[involved],
+                  <div key={index} className='text-[12px] text-white flex flex-row gap-2 items-center gap-1 bg-blue-600 px-1 rounded-md cursor-pointer hover:bg-blue-700'
+                    onClick={() => loadCitizenComponent(involved, theme)}
+                  >
+                    <span><b>{involved.firstname} {involved.lastname}</b> | {cid}</span>
+                    {/* <span className='hover:text-red-500 cursor-pointer hover:bg-blue-800 px-2 rounded-md text-md'
+                      onClick={() => {
+                        // Remove the citizen from the involved list
+                        report.involved[index] = null;
+                        // Update the report
+                        data.reports[report.id] = report;
+                        
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faTimes} />
+                    </span> */}
+                  </div>
+                ))
+              }
+            </div>
           </div>
           <div className="flex flex-row justify-between items-center gap-3">
             <span className="text-gray-400">Creato da <b>{report?.officer}</b></span>
